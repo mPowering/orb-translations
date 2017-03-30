2c69fa9a3720a28ab706e408c18c75621e851aaf
Alex Little <alex@digital-campus.org>
2017-03-30 16:47:05 +0300

    update export script

--- export-script.sh
+++ export-script.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-OUTPUT_DIR="$1"
+LANG="$1"
+OUTPUT_DIR="$2"
 
 # Category model
-python manage.py export_db_translations orb.Category.name > $OUTPUT_DIR/category.po
-    
-    
+python manage.py export_db_translations orb.Category.name --language=$LANG > $OUTPUT_DIR/category.po
+        
 # ResourceCriteria model 
-python manage.py export_db_translations orb.ResourceCriteria.description > $OUTPUT_DIR/resource_criteria.po
+python manage.py export_db_translations orb.ResourceCriteria.description --language=$LANG > $OUTPUT_DIR/resource_criteria.po
 	
 # Resource model
-python manage.py export_db_translations orb.Resource.title orb.Resource.description orb.Resource.attribution > $OUTPUT_DIR/resource.po
-
+python manage.py export_db_translations orb.Resource.title orb.Resource.description orb.Resource.attribution --language=$LANG > $OUTPUT_DIR/resource.po
 
 # ResourceFile model
-python manage.py export_db_translations orb.ResourceFile.title > $OUTPUT_DIR/resource_file.po
+python manage.py export_db_translations orb.ResourceFile.title --language=$LANG > $OUTPUT_DIR/resource_file.po
 	
 # ResourceURL model
-python manage.py export_db_translations orb.ResourceURL.title > $OUTPUT_DIR/resource_url.po
-	
+python manage.py export_db_translations orb.ResourceURL.title --language=$LANG > $OUTPUT_DIR/resource_url.po
 	
 # Tag model
-python manage.py export_db_translations orb.Tag.name > $OUTPUT_DIR/tag.po
+python manage.py export_db_translations orb.Tag.name --language=$LANG > $OUTPUT_DIR/tag.po
